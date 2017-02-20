18695799aa371353a587b867a331b73aae765f43
sergiocandrade <sergiocandrade@gmail.com>
2017-02-20 14:00:51 -0300

    inclusão da lista de ventos de entrega do pedido para o marketplace

---
+++ Epicom.Client/Resources/Marketplace/PedidoEntregaGetListEventosRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Epicom.Http.Client;
+using Epicom.Http.Client.Annotations;
+
+namespace Epicom.Client.Resources.Marketplace
+{
+    [Route("marketplace/pedidos/{IdPedido}/entregas/{IdEntrega}/eventos", "GET")]
+    public class PedidoEntregaGetListEventosRequest : IResponse<IEnumerable<PedidoEntregaEventoGetResponse>>
+    {
+        public long IdPedido { get; set; }
+        public int IdEntrega { get; set; }
+        public int IdEvento { get; set; }
+    }
+}

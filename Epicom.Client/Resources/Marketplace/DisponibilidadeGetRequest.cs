e6fbe0cb8ec8fd9a59e49d3a91aa50a589c1b9f2
Rafael Teixeira <monoman@gmail.com>
2017-03-03 17:25:42 -0300

    Expondo novo entrypoint DisponibilidadeGetRequest/DisponibilidadeGetResponse

---
+++ Epicom.Client/Resources/Marketplace/DisponibilidadeGetRequest.cs
@@ -0,0 +1,12 @@
+﻿using Epicom.Http.Client;
+using Epicom.Http.Client.Annotations;
+
+namespace Epicom.Client.Resources.Marketplace
+{
+    [Route("marketplace/produtos/{IdProduto}/skus/{Id}/disponibilidade", "GET")]
+    public class DisponibilidadeGetRequest : IResponse<DisponibilidadeGetResponse>
+    {
+        public int Id { get; set; }
+        public int IdProduto { get; set; }
+    }
+}
---
+++ Epicom.Client/Resources/Marketplace/DisponibilidadeGetResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Epicom.Client.Resources.Shared;
+
+namespace Epicom.Client.Resources.Marketplace
+{
+	public class DisponibilidadeGetResponse
+	{
+		public decimal Preco { get; set; }
+		public decimal? PrecoDe { get; set; }
+		public bool Disponivel { get; set; }
+		public int? Estoque { get; set; }
+	}
+
+}

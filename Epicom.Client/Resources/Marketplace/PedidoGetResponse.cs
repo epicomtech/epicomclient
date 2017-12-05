df00ee003a2ebffb507907c21b32653222a0559b
Sérgio Andrade <sergio@epicom.com.br>
2017-12-05 10:13:53 -0200

    detalhe do pedido marketplace

--- Epicom.Client/Resources/Marketplace/PedidoGetResponse.cs
+++ Epicom.Client/Resources/Marketplace/PedidoGetResponse.cs
@@ -1,10 +1,43 @@
-﻿using Epicom.Http.Client;
+﻿using Epicom.Client.Resources.Shared;
+using Epicom.Http.Client;
 using Epicom.Http.Client.Annotations;
+using System;
+using System.Collections.Generic;
 
 namespace Epicom.Client.Resources.Marketplace
 {
     public class PedidoGetResponse
-    {
-        public string Status { get; set; }
-    }
+    {        
+		public long Id { get; set; }
+
+		public DateTime DataAbertura { get; set; }
+
+		public decimal ValorTotal { get; set; }
+
+		public List<Sku> Itens { get; set; }
+
+		public PedidoDestinatario Destinatario { get; set; }
+
+		public string CodigoPedidoMarketplace { get; set; }
+
+		public PedidoEndereco Endereco { get; set; }
+
+		public string Status { get; set; }
+
+		public class Sku
+		{
+			public int Id { get; set; }
+			public int Quantidade { get; set; }
+			public decimal Valor { get; set; }
+			public decimal ValorFrete { get; set; }
+			public string CodigoFornecedor { get; set; }
+			public string NomeFornecedor { get; set; }
+			public string CodigoPedidoFornecedor { get; set; }
+			public List<string> IdsItensMarketplace { get; set; }
+
+			public string FormaEntrega { get; set; }
+
+			public string PrazoEntrega { get; set; }
+		}
+	}
 }

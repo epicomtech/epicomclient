2e40145611b995e92bbb24175c734641ef3e34ad
epicomvanessasalvador <vanessa@epicom.com.br>
2017-12-20 13:54:07 -0200

    alterando get, post e patch das entregas e eventos para buscar, criar e a lterar pelo idPedido e idEntrega

--- Epicom.Client/Resources/Fornecedor/PedidoEntregaGetRequest.cs
+++ Epicom.Client/Resources/Fornecedor/PedidoEntregaGetRequest.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 
 namespace Epicom.Client.Resources.Fornecedor
 {
-    [Route("fornecedor/pedidos/{CodigoPedido}/entregas", "GET")]
+    [Route("fornecedor/pedidos/{IdPedido}/entregas", "GET")]
     public class PedidoEntregaGetRequest : IResponse<IEnumerable<PedidoEntregaGetResponse>>
     {
-        public string CodigoPedido { get; set; }
+        public long IdPedido { get; set; }
     }
 }

7b4b32a75a3a89168a0071eeabc5461d0a3f2db6
Sérgio Andrade <sergio@epicom.com.br>
2018-01-09 20:49:13 -0200

    incluindo id no post da entrega

---
+++ Epicom.Client/Resources/Fornecedor/PedidoEntregaPostResponse.cs
@@ -0,0 +1,12 @@
+﻿using Epicom.Http.Client;
+using Epicom.Http.Client.Annotations;
+using System;
+using System.Collections.Generic;
+
+namespace Epicom.Client.Resources.Fornecedor
+{    
+    public class PedidoEntregaPostResponse
+    {
+        public long Id { get; set; }        
+    }
+}

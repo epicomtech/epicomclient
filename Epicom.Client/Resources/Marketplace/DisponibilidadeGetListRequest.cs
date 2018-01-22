9ea1efa94175ad92077d9d1e60f9d605af1d32a3
Vanessa Salvador <vanessa@epicom.com.br>
2018-01-22 19:47:26 -0200

    corrigindo list disponibiilidade

--- Epicom.Client/Resources/Marketplace/DisponibilidadeGetListRequest.cs
+++ Epicom.Client/Resources/Marketplace/DisponibilidadeGetListRequest.cs
@@ -1,11 +1,13 @@
-﻿using Epicom.Http.Client;
+﻿using Epicom.Client.Resources.Shared;
+using Epicom.Http.Client;
 using Epicom.Http.Client.Annotations;
 using System;
+using System.Collections.Generic;
 
 namespace Epicom.Client.Resources.Marketplace
 {
-    [Route("marketplace/disponibilidades", "GET")]
-    public class DisponibilidadeGetListRequest : IResponse<DisponibilidadeGetListResponse>
+    [Route("marketplace/disponibilidades?&DataInicio={DataInicio}&DataFim={DataFim}&Offset={Offset}&Limit={Limit}", "GET")]
+    public class DisponibilidadeGetListRequest : PagedRequest, IResponse<IEnumerable<DisponibilidadeGetListResponse>>
     {
         public DateTime? DataInicio { get; set; }
         public DateTime? DataFim { get; set; }

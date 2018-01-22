d5e4b2c4a2473deb7666b1e267c4a583bb9e0b34
Vanessa Salvador <vanessa@epicom.com.br>
2018-01-22 16:48:12 -0200

    criando request e response da disponibilidade marketplace

---
+++ Epicom.Client/Resources/Marketplace/DisponibilidadeGetListRequest.cs
@@ -0,0 +1,12 @@
+﻿using Epicom.Http.Client;
+using Epicom.Http.Client.Annotations;
+
+namespace Epicom.Client.Resources.Marketplace
+{
+    [Route("marketplace/disponibilidades", "GET")]
+    public class DisponibilidadeGetListRequest : IResponse<DisponibilidadeGetResponse>
+    {
+        public int DataInicio { get; set; }
+        public int DataFim { get; set; }
+    }
+}

466198f2f1c75dfeb7c441891d60d3854e5cff50
Vanessa Salvador <vanessa@epicom.com.br>
2018-01-22 17:08:12 -0200

    request e response disponibilidade marketplacw

---
+++ Epicom.Client/Resources/Marketplace/DisponibilidadeGetListRequest.cs
@@ -0,0 +1,13 @@
+﻿using Epicom.Http.Client;
+using Epicom.Http.Client.Annotations;
+using System;
+
+namespace Epicom.Client.Resources.Marketplace
+{
+    [Route("marketplace/disponibilidades", "GET")]
+    public class DisponibilidadeGetListRequest : IResponse<DisponibilidadeGetListResponse>
+    {
+        public DateTime? DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+    }
+}
---
+++ Epicom.Client/Resources/Marketplace/DisponibilidadeGetListResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Epicom.Client.Resources.Shared;
+
+namespace Epicom.Client.Resources.Marketplace
+{
+	public class DisponibilidadeGetListResponse
+	{
+        public int SkuId { get; set; }
+        public string CodigoEmpresa { get; set; }
+        public decimal? Preco { get; set; }
+        public decimal? PrecoDe { get; set; }
+    }
+
+}

a825fe481564d1a836a96503bc8570b2a9cf943a
Vanessa Salvador <vanessa@epicom.com.br>
2018-01-23 09:00:01 -0200

    incluindo produtoId no DisponibilidadeGetListResponse

--- Epicom.Client/Resources/Marketplace/DisponibilidadeGetListResponse.cs
+++ Epicom.Client/Resources/Marketplace/DisponibilidadeGetListResponse.cs
@@ -6,6 +6,7 @@ namespace Epicom.Client.Resources.Marketplace
 	public class DisponibilidadeGetListResponse
 	{
         public int SkuId { get; set; }
+        public int ProdutoId { get; set; }
         public string CodigoEmpresa { get; set; }
         public decimal? Preco { get; set; }
         public decimal? PrecoDe { get; set; }

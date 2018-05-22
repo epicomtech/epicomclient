5bef3dd3b5ba3b12fcfe43d162aaa0bccce8e40f
Sergio Andrade <sergio@epicom.com.br>
2018-05-22 15:06:49 -0300

    adicionando id na respsota do produto

--- Epicom.Client/Resources/Fornecedor/ProdutoGetResponse.cs
+++ Epicom.Client/Resources/Fornecedor/ProdutoGetResponse.cs
@@ -5,6 +5,7 @@ namespace Epicom.Client.Resources.Fornecedor
 {
     public class ProdutoGetResponse
     {
+        public int Id { get; set; }
         public string Codigo { get; set; }
         public string Nome { get; set; }
         public string NomeReduzido { get; set; }

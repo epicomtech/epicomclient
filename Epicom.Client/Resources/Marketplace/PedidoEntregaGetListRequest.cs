96fd19b368cd2e5dacfdbe3aadfdaa9d2acaae1f
Sergio Andrade <sergio@epicom.com.br>
2018-08-14 12:12:44 -0300

    entregas marketplace

--- Epicom.Client/Resources/Marketplace/PedidoEntregaGetListRequest.cs
+++ Epicom.Client/Resources/Marketplace/PedidoEntregaGetListRequest.cs
@@ -1,10 +1,11 @@
-﻿using Epicom.Http.Client;
+﻿using System.Collections.Generic;
+using Epicom.Http.Client;
 using Epicom.Http.Client.Annotations;
 
 namespace Epicom.Client.Resources.Marketplace
 {
     [Route("marketplace/pedidos/{IdPedido}/entregas", "GET")]
-    public class PedidoEntregaGetListRequest : IResponse<PedidoEntregaGetResponse>
+    public class PedidoEntregaGetListRequest : IResponse<IEnumerable<PedidoEntregaGetResponse>>
     {
         public long IdPedido { get; set; }        
     }

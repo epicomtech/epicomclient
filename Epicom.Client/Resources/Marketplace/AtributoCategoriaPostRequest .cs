281f4eb515d5239d9b6aa170c529dc6838f698e6
Sergio Andrade <sergio@epicom.com.br>
2018-09-26 14:43:26 -0300

    alteração no post de atrbutos

---
+++ Epicom.Client/Resources/Marketplace/AtributoCategoriaPostRequest .cs
@@ -0,0 +1,19 @@
+﻿using Epicom.Http.Client;
+using Epicom.Http.Client.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epicom.Client.Resources.Marketplace
+{
+    [Route("marketplace/atributos", "POST")]
+    public class AtributoCategoriaPostRequest : IEmptyResponse
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public bool AtributoValorLivre { get; set; }        
+                
+    }
+}

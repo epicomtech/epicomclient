aa70f2a6c06dce325b3cfee7d4ed46aab9f19186
Vanessa Salvador <vanessa@epicom.com.br>
2019-03-13 15:32:40 -0300

    ajustes skupatchRequest e SkuPostRequest

--- Epicom.Client/Resources/Fornecedor/SkuPostRequest.cs
+++ Epicom.Client/Resources/Fornecedor/SkuPostRequest.cs
@@ -25,7 +25,7 @@ namespace Epicom.Client.Resources.Fornecedor
         public DimensoesRequest Dimensoes { get; set; }
         public IEnumerable<ImagemPostRequest> Imagens { get; set; }
         public IEnumerable<GrupoPostRequest> Grupos { get; set; }
-        public IEnumerable<GrupoGetResponse.Atributo> Variacoes { get; set; }
+        public IEnumerable<GrupoPostRequest.Atributo> Variacoes { get; set; }
 
     }
 }
